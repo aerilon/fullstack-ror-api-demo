68cb2e1b4c34b3374190186617d5d3b9f97cade1
Arnaud Lacombe <lacombar@gmail.com>
2018-03-25 22:16:38 -0700

    config/routes.rb: setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    resources :product, only: [:create] # TODO: :destroy & :update
+    resource :products, only: [:show]
+  end
 end
