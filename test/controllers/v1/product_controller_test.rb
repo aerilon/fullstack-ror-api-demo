f847fec64677ad3aba29910c417b0a320ad15b13
Arnaud Lacombe <lacombar@gmail.com>
2018-03-27 22:25:57 -0700

    product_controller_test: add outline

--- test/controllers/v1/product_controller_test.rb
+++ test/controllers/v1/product_controller_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class V1::ProductControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+# TODO test product insersion on POST
+
+# TODO test that subsequent POST request on the same ASIN return 303
+
+# TODO test product retrieval on GET
+
+# TODO test abscence of `created_at' and `updated_at' from returned records
+
 end
